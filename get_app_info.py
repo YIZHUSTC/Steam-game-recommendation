a1ad5693c192260e7e75d3b66124ef0ed2f640a4
YIZHUSTC <ustcmark@gmail.com>
2019-02-20 00:43:33 -0800

    Add files via upload

---
+++ get_app_info.py
@@ -0,0 +1,29 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext, SparkSession, Row
+from pyspark.sql.types import StructField, StructType, IntegerType, StringType, LongType, DoubleType, DateType
+import pyspark.sql.functions as f
+import requests, json, os, sys, time, re
+
+def get_app_detail(appid):
+    for i in range(3):
+        try:
+            r = requests.get('http://store.steampowered.com/api/appdetails?appids=' + str(appid)).json()
+            if r.get(appid).get('success'):
+                time.sleep(0.5)
+                detail = r.get(appid).get('data')
+                return detail
+        except:
+            time.sleep(5)
+            pass
+
+
+def get_app_info():
+    for i in range(3):
+        try: 
+            r = requests.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/').json()
+            apps = r.get('applist').get('apps')
+            appid = sc.parallelize(apps).map(lambda x : x.get('appid'))
+            app_detail = appid.map(lambda x : get_app_detail(str(x))).filter(lambda x : x).cache()
+            return app_detail
+        except:
+            time.sleep(5)
