4e276111b714e02cf6532f7b3379b0753e464d04
YIZHUSTC <ustcmark@gmail.com>
2019-02-24 18:16:28 -0800

    Update als_based.py

--- als_based.py
+++ als_based.py
@@ -20,8 +20,8 @@ def als_model():
 	    userid = r[0]
 	    idList = r[1]
 	    spark.sql("INSERT INTO als_top20 ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s',\
-												'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % \
-										 (userid, idList[0], idList[1], idList[2], idList[3], idList[4], idList[5], \
-												  idList[6], idList[7], idList[8], idList[9], idList[10], \
-												  idList[11], idList[12], idList[13], idList[14], idList[15], \
-												  idList[16], idList[17], idList[18], idList[19]))
+						    '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % \
+					     (userid, idList[0], idList[1], idList[2], idList[3], idList[4], idList[5], \
+						      idList[6], idList[7], idList[8], idList[9], idList[10], idList[11], \
+					              idList[12], idList[13], idList[14], idList[15], idList[16], \
+					              idList[17], idList[18], idList[19]))
